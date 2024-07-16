b6efc395b441c43f3920d97e565b1ce91615ff92
anshulkardam <anshulkardamsr@gmail.com>
2024-07-16 18:57:32 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { RevenueCard } from './Components/Revenuecard'
+import { Navbar } from './Components/Navbar'
+
 
 function App() {
 
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(async res => {
+        const data = await res.json();
+        setPosts(data)
+
+      })
+
+  }, [])
+
+
+
   return (
-    <div className='grid grid-cols-3'>
-<RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={"13"}/>
-</div>
+     <>
+        <Navbar />
+         <div className='flex flex-wrap gap-8'>
+      {posts.map((data) => (
+        
+        <div key={data.id} className='border border-black w-96 '>
+          <h1>User {data.userId}</h1>
+          <h2>title: {data.title}</h2>
+          <h4>body: {data.body}</h4>
+        </div>
+       
+        ))}
+    </div>
+    </>
+
   )
 }
+
 
 
 export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+export function Navbar(){
+
+    return (
+        <div className="bg-purple-400 ">
+            <ul className="flex justify-center space-x-3">
+                <li>Home</li>
+                <li>about</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
